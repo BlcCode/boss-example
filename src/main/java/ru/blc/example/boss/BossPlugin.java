509152a6fda3616c1a1459d3f3b40c5c054139d2
Blc <fenix785632@gmail.com>
2021-11-02 16:35:28 +0500

    feat: plugin startup logic

--- src/main/java/ru/blc/example/boss/BossPlugin.java
+++ src/main/java/ru/blc/example/boss/BossPlugin.java
@@ -1,10 +1,89 @@
 package ru.blc.example.boss;
 
+import com.google.common.base.Charsets;
+import lombok.Getter;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class BossPlugin extends JavaPlugin {
 
+    private File messagesFile;
+    private FileConfiguration messagesConfiguration;
+    @Getter
+    private SqlConnection sqlConnection;
+
+    //bukkit methods
+    @Override
+    public void saveDefaultConfig() {
+        if (!messagesFile.exists()) {
+            this.saveResource("messages.yml", false);
+        }
+        super.saveDefaultConfig();
+    }
+
+    @Override
+    public void reloadConfig() {
+        this.messagesConfiguration = YamlConfiguration.loadConfiguration(this.messagesFile);
+        InputStream defConfigStream = this.getResource("messages.yml");
+        if (defConfigStream != null) {
+            this.messagesConfiguration.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(defConfigStream, Charsets.UTF_8)));
+        }
+        super.reloadConfig();
+    }
+
+    @Override
+    public void saveConfig() {
+        //nothing
+    }
+
+    //plugin logic
+    @Override
+    public void onLoad() {
+        messagesFile = new File(getDataFolder(), "messages.yml");
+
+        saveDefaultConfig();
+        reloadConfig();
+    }
+
     @Override
     public void onEnable() {
+        if (!createDatabaseConnection()) {
+            //creation error logs by SqlConnection.class
+            getSLF4JLogger().error("Plugin will disabled!");
+            setEnabled(false);
+            return;
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        if (sqlConnection != null) {
+            sqlConnection.close();
+        }
+    }
+
+    //other
+
+    public FileConfiguration getMessagesConfiguration() {
+        if (messagesConfiguration == null) reloadConfig();
+        return messagesConfiguration;
+    }
+
+    //private
+    private boolean createDatabaseConnection() {
+        sqlConnection = new SqlConnection(this);
+        //store database credentials at config is very dangerous, and requires configuring database for each plugin
+        String host = Objects.requireNonNull(System.getenv("SQL_HOST"), "Specify SQL_HOST env");
+        String port = Objects.requireNonNullElse(System.getenv("SQL_PORT"), "3306");
+        String user = Objects.requireNonNull(System.getenv("SQL_USER"), "Specify SQL_USER env");
+        String password = Objects.requireNonNull(System.getenv("SQL_PASSWORD"), "Specify SQL_PASSWORD env");
+        String database = getConfig().getString("database", "");
+        return !database.isBlank() && sqlConnection.connect(host, port, user, password, database);
     }
 }
---
+++ src/main/java/ru/blc/example/boss/SqlConnection.java
@@ -0,0 +1,93 @@
+package ru.blc.example.boss;
+
+import lombok.NonNull;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class SqlConnection {
+
+    private final JavaPlugin plugin;
+    private Connection connection;
+
+    public SqlConnection(@NonNull JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    /**
+     * Create connection to database with specified params
+     *
+     * @return true if connection successful, otherwise false
+     */
+    public boolean connect(@NotNull String host, String port, @NotNull String username, @NotNull String password, @NotNull String databaseName) {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + databaseName + "?useUnicode=true&characterEncoding=utf8&autoReconnect=true", username, password);
+            return true;
+        } catch (SQLException ex) {
+            plugin.getSLF4JLogger().error("Connection to database failed!", ex);
+            return false;
+        }
+    }
+
+    public void close() {
+        if (connection == null) return;
+        try {
+            connection.close();
+        } catch (SQLException throwables) {
+            plugin.getSLF4JLogger().error("Disconnection from database failed!", throwables);
+        }
+    }
+
+    /**
+     * Creates statement
+     *
+     * @param sql  sql expression
+     * @param args arguments
+     * @return {@link Optional} with prepared statement, or empty if there is errors while statement creating
+     */
+    public @NotNull Optional<PreparedStatement> prepareStatement(String sql, Object... args) {
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
+            }
+            return Optional.of(statement);
+        } catch (SQLException throwables) {
+            plugin.getSLF4JLogger().error("Failed to prepare statement!", throwables);
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Executes specified statement and close it
+     *
+     * @param statement statement for execution
+     */
+    @Contract(mutates = "param1")
+    public void executeUpdate(PreparedStatement statement) {
+        try {
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException throwables) {
+            plugin.getSLF4JLogger().error("Query execution failed!", throwables);
+        }
+    }
+
+    /**
+     * Runs {@link SqlConnection#executeUpdate(PreparedStatement)} at async task
+     */
+    public void executeUpdateAsync(PreparedStatement statement) {
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> executeUpdate(statement));
+    }
+
+}
